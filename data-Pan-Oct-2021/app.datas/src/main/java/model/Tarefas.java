d63263f1694a54b13cd91d938cf41fbb62597521
marcelpinotti <marcel.pinotti@gmail.com>
2021-10-14 22:19:37 -0300

    working date

---
+++ data-Pan-Oct-2021/app.datas/src/main/java/model/Tarefas.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Tarefas {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	@Column (nullable = false)	
+	private String tarefa;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar dataNascimentoCalendar;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate date;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTarefa() {
+		return tarefa;
+	}
+
+	public void setTarefa(String tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public Calendar getDataNascimentoCalendar() {
+		return dataNascimentoCalendar;
+	}
+
+	public void setDataNascimentoCalendar(Calendar dataNascimentoCalendar) {
+		this.dataNascimentoCalendar = dataNascimentoCalendar;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+
+}
